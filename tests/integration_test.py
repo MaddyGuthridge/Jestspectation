dab65fa69c677520c43c7f08922551e1b5eff239
Miguel Guthridge <hdsq@outlook.com.au>
2022-11-12 05:30:15 +1100

    Add an extra test case

--- tests/integration_test.py
+++ tests/integration_test.py
@@ -31,3 +31,39 @@ def test_nested_match():
         }),
         "my_list": expect.ListContaining([3]),
     }
+
+
+def test_advanced():
+    my_dict = {
+        "foo": "Hello",
+        "bar": 1.002,
+        "baz": [
+            {
+                "this": 1,
+                "shouldn't": 2,
+                "match": 3,
+            },
+            {
+                "some_value": True,
+                "other_value": "Hello",
+                "yet_another_value": {
+                    "contents": 42,
+                    "more_contents": "Hello",
+                }
+            }
+        ]
+    }
+    expected = expect.DictContainingItems({
+        "foo": expect.Any(str),
+        "bar": expect.FloatApprox(1, percent=1),
+        "baz": expect.ListContaining([{
+            "some_value": expect.Any(bool),
+            "other_value": "Hello",
+            "yet_another_value": {
+                "contents": expect.Any(int),
+                "more_contents": expect.Not(
+                    expect.StringMatchingRegex("Goodbye*"))
+            }
+        }])
+    })
+    assert expected == my_dict
